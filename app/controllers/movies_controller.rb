0247da2218a1d06adbe97e10a3e2032f84749843
Steven Brown <sgbrown@mac.com>
2015-04-03 12:19:29 +1300

    index controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
 
   def index
     redirect_to new_user_session_path unless user_signed_in?
-    @movies = Movie.all
+    @data =
   end
 
   def search
