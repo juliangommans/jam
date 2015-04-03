cf140ebf1ac6fc97f3b3daad1d917297a8a7bab8
Sandra <mursan03@gmail.com>
2015-04-03 00:58:17 +0000

    cache issues

--- app/assets/javascripts/JAM.js
+++ app/assets/javascripts/JAM.js
@@ -10,7 +10,7 @@ var videoId = 81707;
     function LoadTrailer() {
       $('.viewTrailer').children().remove();
       $('.viewTrailer').append('<iframe width="640" height="390 src="http://v.traileraddict.com/'+videoId+
-            '?autoplay=1"" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no" frameborder="1"></iframe>');
+            '?autoplay=1" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no" frameborder="1"></iframe>');
     }; 
     
     function Upcoming(){
