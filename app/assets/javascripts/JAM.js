8b15fb1ecca10029f0545c8ec71d7af66ebf6c34
Sandra <mursan03@gmail.com>
2015-04-03 01:52:23 +0000

    videoplayer

--- app/assets/javascripts/JAM.js
+++ app/assets/javascripts/JAM.js
@@ -8,18 +8,11 @@ $(document).ready(function(){
 var videoId = 81707;
 
     function LoadTrailer() {
-<<<<<<< HEAD
-      //$('.viewTrailer').children().remove();
-    //  $('.viewTrailer').html('<p><iframe width="640" height="390 src="http://v.traileraddict.com/'+videoId+
-     //     '?autoplay=1" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no" frameborder="1"></iframe></p>');
-          
-           $('.viewTrailer').html("<p> hi</p>")
-          
-=======
       $('.viewTrailer').children().remove();
-      $('.viewTrailer').append('<iframe width="640" height="390 src="http://v.traileraddict.com/'+videoId+
-            '?autoplay=1" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no" frameborder="1"></iframe>');
->>>>>>> 76bdcc000a61d19b6615f738f383c003071bc89a
+      $('.viewTrailer').append('<p><iframe width="640" height="390" src="http://v.traileraddict.com/'+videoId+
+          '?autoplay=1" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no" frameborder="1"></iframe></p>');
+          
+
     }; 
     
     function Upcoming(){
