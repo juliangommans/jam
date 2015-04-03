88d0c7ff83389c08e7c7fabef2806e760205088f
juliangommans <julian.s.gommans@gmail.com>
2015-04-03 13:27:41 +1300

    fixed apis

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root 'movies#index'
   get '/seach' => 'movies#search', as: 'movie_search'
+  get '/trailer' => 'movies#trailer', as: 'movie_trailer'
+  get '/upcoming' => 'movies#upcoming', as: 'movie_upcoming'
   post '/add/:id' => 'jamjams#add', as: 'add_movie'
   post '/remove/:id' => 'jamjams#remove', as: 'remove_movie'
   post '/watched/:id' => 'jamjams#watched', as: 'watch_movie'
