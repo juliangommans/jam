f3b3f071cade0d5ea316e79705d2c7f5d667350f
Sandra <mursan03@gmail.com>
2015-04-04 21:29:49 +0000

    remove item on a double click??

--- app/assets/javascripts/JAM.js
+++ app/assets/javascripts/JAM.js
@@ -101,8 +101,7 @@ var j= 0;
     
     function Remove() {
         $('#remove').click(function(){
-            var item = $(this).attr('id');
-            $('.myList').remove('#'+item+''); 
+            $(this).closest('li').remove();
         });
 
     }
