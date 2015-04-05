9e354f681d51001683f8dd0ac14b73c8b8128f15
Steven Brown <sgbrown@mac.com>
2015-04-06 11:25:27 +1200

    admin users created in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,3 @@
-require 'faker'
-
-Movie.delete_all
-Moviejam.delete_all
-
-random_movies = []
-
-10.times do |i|
-  random_movies << Movie.create(title: Faker::Commerce.product_name).id
-end
-
-50.times do |j|
-  random_watched = rand(1..5) == 1 ? true : false
-  Moviejam.create(user_id: rand(1..5), movie_id: random_movies.sample, watched: random_watched, prerating: rand(0..100), postrating: rand(0..100))
-end
+User.create!({:name => "Steven Brown", :email => "sgbrown.nz@gmail.com", :admin => true, :password => "MovieJamAdmin", :password_confirmation => "MovieJamAdmin"})
+User.create!({:name => "Sandra Muller", :email => "mursan03@gmail.com", :admin => true, :password => "MovieJamAdmin", :password_confirmation => "MovieJamAdmin"})
+User.create!({:name => "Julian Gommans", :email => "julian.s.gommans@gmail.com", :admin => true, :password => "MovieJamAdmin", :password_confirmation => "MovieJamAdmin"})
