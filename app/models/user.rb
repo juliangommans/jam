80a227b23ba5efc4b2f0b3d7b7495bb005bdda85
Steven Brown <sgbrown@mac.com>
2015-04-07 16:14:44 +1200

    no duplicate users' names)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :moviejams
+  validates_uniqueness_of :name
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable, :recoverable
