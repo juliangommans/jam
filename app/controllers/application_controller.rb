65bbd1324fab28449666ce1bf7d947fa8231b170
Steven Brown <sgbrown@mac.com>
2015-04-08 10:29:29 +1200

    Feature page link to random movie from db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :feature_movie
+
+  def feature_movie
+    @movies = Movie.all
+    @movie = @movies.sample
+  end
+
 end
